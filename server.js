08db9aa16cab74d075247f1054ee3ba9533b9bef
Brittney <brittneyrichardson8@gmail.com>
2022-05-15 10:51:20 -0500

    completed app post

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 
+const uuid = require('./helpers/uuid');
+
 const PORT = process.env.PORT || 3001;
 const app = express()
 
@@ -23,28 +25,52 @@ app.get('/api/notes', (req, res) => {
 
 
 app.post('/api/notes', (req, res) => {
- fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
+    console.log(`${req.method} request received to add new note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+
+        }; fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new review
+                parsedNotes.push(newNote);
+
+                // Write updated reviews back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
     } else {
-      // Convert string into JSON object
-      const parsedReviews = JSON.parse(data);
-
-      // Add a new review
-      parsedReviews.push(newReview);
-
-      // Write updated reviews back to the file
-      fs.writeFile(
-        './db/db.json',
-        JSON.stringify(parsedReviews, null, 4),
-        (writeErr) =>
-          writeErr
-            ? console.error(writeErr)
-            : console.info('Successfully updated reviews!')
-      );
-   }
- });
+        res.json('Error posting new note');
+    }
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+
+})
 //api routes end
 
 
